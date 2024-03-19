ac4f5883cef0c6876fab153f6fb15898a39f158f
JulianEadie <80416799+JulianEadie@users.noreply.github.com>
2024-03-19 10:13:03 -0400

    Update honoi.py

--- honoi.py
+++ honoi.py
@@ -1,1 +1,26 @@
+from rings import *
+from the_forgotten import *
+from colorama import Fore
+import os
+
+def clear_screen():
+  os.system('cls' if os.name == 'nt' else 'clear')
+
+def pegs():
+    print(Fore.RED + 'THE NUMBER OF PEGS WILL NEVER CHANGE!')
+    clear_screen()
+    print(Fore.RED + 'THE NUMBER OF PEGS WILL ALWAYS BE THREE PEGS')
+    clear_screen()
+    print(Fore.RED + 'THE ONLY THING THAT WILL CHANGE IS THE NUMBER OF RINGS')
+    clear_screen()
+    rules = input(Fore.RED + 'DO YOU UNDERSTAND THESE CONDITIONS? (y/n): ')
+    if rules.lower() != "y":
+       print(Fore.GREEN + 'Thank you for accepting our conditions!')
+       clear_screen()
+       rings()
+    else:
+       the_forgotten()
+
+
+
 
